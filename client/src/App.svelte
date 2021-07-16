0a62ba6852af62efb756495cd738a3ca14d4f1b6
Gerardo115pp <theronin115@gmail.com>
2021-07-16 03:40:08 -0500

    NavBar and hero done

---
+++ client/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+    import Router from 'svelte-spa-router';
+    import { routes } from './routes';
+</script>
+
+<style>
+    @import url(//db.onlinewebfonts.com/c/58aaccbcd294a88d9b91af291895060d?family=GatsbyFLF);
+
+    :global(:root) {
+        --theme-color: #FF9900;
+        --theme-font: GatsbyFLF
+    }    
+
+    /*=============================================
+    =            Normalization            =
+    =============================================*/
+    
+    :global() {
+        margin: 0;
+    }
+
+    :global(body) {
+        font-family: var(--theme-font), sans-serif;
+        margin: 0;
+    }
+    
+    /*=====  End of Normalization  ======*/
+    
+    
+    #main-component {
+        font-family: var(--theme-font);
+        color: var(--theme-color);
+    }
+</style>
+
+<svelte:head>
+    <link type="text/css" rel="stylesheet" href="normalize.css" />
+</svelte:head>
+
+<div id="main-component">
+    <Router {routes} />
+</div>
---
+++ client/src/components/ultra-navbar.svelte
@@ -0,0 +1,52 @@
+<script>
+    import ultra_janitorial_logo from '../images/logo.svg';
+</script>
+
+<style>
+    
+    /* main nav bar container style */
+    #uj-navbar {
+        display: flex;
+        background: #fff;
+        border-bottom: 1px solid var(--theme-color);
+        justify-content: space-between;
+        padding: .7vh 1.3vw;
+        box-shadow: 0 0 10px 5px rgba(0,0,0,.05);
+    }
+
+    :global(#ultra-janitorial-nav-logo svg) {
+        width: 5vw;
+    }
+
+    /* nav bar style */
+    #right-content {
+        display: flex;
+        width: 30%;
+        font-size: 1.5rem;
+        align-items: center;
+    }
+    
+    #right-content span {
+        cursor: pointer;
+        margin-right: 4em;
+    }
+</style>
+
+<div id="uj-navbar" class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div id="ultra-janitorial-nav-logo">
+        <a href="#uj-navbar">
+            {@html ultra_janitorial_logo}
+        </a>
+    </div>
+    <div id="right-content">
+        <span class="nav-content">
+            Services
+        </span>
+        <span class="nav-content">
+            Contact Us
+        </span>
+        <span class="nav-content">
+            About Us
+        </span>
+    </div>
+</div>
